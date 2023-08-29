7e18ed37104b64b9397fe7d08a070e6bc1e15063
Lidia Cristian <lidia.ioana@gmail.com>
2023-08-29 10:46:02 +0300

    assignment commit

--- C#basics/C#basics/Assignment2.cs
+++ C#basics/C#basics/Assignment2.cs
@@ -8,5 +8,27 @@ namespace C_basics
 {
     internal class Assignment2
     {
+        public static void SumOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine("Sum of " + firstNumber + "+" + secondNumber + " is: " + sum);
+        }
+
+        public static void SubstractionOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int sub = firstNumber - secondNumber;
+            Console.WriteLine("Substraction of " + firstNumber + "-" + secondNumber + " is: " + sub);
+        }
+
+        public static void MultiplicationOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            float rez = (float)firstNumber * (float)secondNumber;
+            Console.WriteLine("Multiplication of " + firstNumber + "*" + secondNumber + " is: " + rez);
+        }
+        public static void DivisionOfTwoNumbers(int firstNumber, int secondNumber)
+        {
+            float rez = (float)firstNumber / (float)secondNumber;
+            Console.WriteLine("Divizion of " + firstNumber + "/" + secondNumber + " is: " + rez);
+        }
     }
 }
